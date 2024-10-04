9bfb3a26f295c542ee58ce5cd83fa73948d10e0c
Alon Alexander <alon008@gmail.com>
2024-10-04 15:31:10 +0300

    Add option to use a single attention call with big mask

--- graphgps/layer/custom_attention.py
+++ graphgps/layer/custom_attention.py
@@ -6,15 +6,37 @@ from cdlib import algorithms
 import networkx as nx
 
 
+def diagonalize_3d(matrices):
+    shape = torch.Size([
+        sum(mat.shape[0] for mat in matrices),
+        sum(mat.shape[1] for mat in matrices),
+        matrices[0].shape[2]
+    ])
+
+    out = torch.zeros(shape)
+    
+    i_cur = 0
+    j_cur = 0
+    for mat in matrices:
+        i_size, j_size, *_ = mat.shape
+        out[i_cur:i_cur + i_size, j_cur:j_cur + j_size, :] = mat
+        i_cur += i_size
+        j_cur += j_size
+
+    return out
+
+
 class CustomAttention(nn.Module):
     def __init__(self, 
                  dim_h: int, 
                  num_heads: int,
-                 attn_dropout=0.0
+                 attn_dropout=0.0,
+                 operate_individually: bool = True,
                  ):
         super().__init__()
         self.attn = torch.nn.MultiheadAttention(
             dim_h, num_heads, dropout=attn_dropout, batch_first=True)
+        self.operate_individually = operate_individually
     
     def forward(self, batch):
         # Transform to networkx graph
@@ -22,6 +44,10 @@ class CustomAttention(nn.Module):
 
         # Separate into communities
         coms = algorithms.walktrap(G)
+
+        # For joining later
+        dense_subgraphs = []
+        dense_masks = []
 
         for community in coms.communities:
             subgraph = G.subgraph(community)
@@ -32,22 +58,32 @@ class CustomAttention(nn.Module):
                 torch_subgraph.x, 
                 torch_subgraph.batch
                 )
-            torch_subgraph.x = self.attn.forward(
-                dense_subgraph, dense_subgraph, dense_subgraph, 
-                attn_mask=None, 
-                key_padding_mask=~dense_mask, 
-                need_weights=False,
-            )[0][dense_mask]
+            if self.operate_individually:
+                torch_subgraph.x = self.attn.forward(
+                    dense_subgraph, dense_subgraph, dense_subgraph, 
+                    attn_mask=None, 
+                    key_padding_mask=~dense_mask, 
+                    need_weights=False,
+                )[0][dense_mask]
 
-            # Copy data back to networkx graph from community
-            for i, node in enumerate(subgraph.nodes):
-                G.nodes[node]['x'] = torch_subgraph.x[i].tolist()
+                # Copy data back to networkx graph from community
+                for i, node in enumerate(subgraph.nodes):
+                    G.nodes[node]['x'] = torch_subgraph.x[i].tolist()
 
-        data_after_communities = from_networkx(G)
+            else:
+                dense_subgraphs.append(dense_subgraph)
+                dense_masks.append(dense_mask)
 
-        return data_after_communities.x
-        # return self.attn.forward(h_dense, h_dense, h_dense, 
-        #                          attn_mask=None, 
-        #                          key_padding_mask=~mask, 
-        #                          need_weights=False,
-        #                          )[0][mask]
+
+        if self.operate_individually:
+            data_after_communities = from_networkx(G)
+            return data_after_communities.x
+        else:
+            joint_dense = diagonalize_3d(dense_subgraphs)
+            joint_mask = torch.block_diag(*dense_masks)
+
+            return self.attn.forward(joint_dense, joint_dense, joint_dense, 
+                                    attn_mask=None, 
+                                    key_padding_mask=~joint_mask, 
+                                    need_weights=False,
+                                    )[0][joint_mask]
